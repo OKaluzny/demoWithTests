b28d17e86599019c26dae5ca7c9bd3a47cbf8375
Viacheslav Dubovskyi <architect1slava@gmail.com>
2022-08-25 13:15:47 +0300

    Lombok, phoneNumber and isDeleted

--- src/main/java/com/example/demowithtests/domain/Employee.java
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -3,11 +3,15 @@ package com.example.demowithtests.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -20,45 +24,7 @@ public class Employee {
     private String name;
     private String country;
     private String email;
+
+    private Integer phoneNumber;
     private boolean isDeleted;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
 }
