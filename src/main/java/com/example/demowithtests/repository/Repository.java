39e000e2f5bc1dd44c8c1fcf723a34502c59e1bc
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-06 22:04:14 +0300

    add method

--- src/main/java/com/example/demowithtests/repository/Repository.java
+++ src/main/java/com/example/demowithtests/repository/Repository.java
@@ -2,14 +2,18 @@ package com.example.demowithtests.repository;
 
 import com.example.demowithtests.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 @org.springframework.stereotype.Repository
 //@Component
 public interface Repository extends JpaRepository<Employee, Integer> {
 
     Employee findByName(String name);
+
+    @Query(value = "SELECT * FROM users WHERE name = ?1", nativeQuery = true)
+    List<Employee> find(String name);
+
 
 }
