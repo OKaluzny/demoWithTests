39efe9d686e84ad086cb6d985f412b4198e74a42
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-19 20:48:58 +0300

    some changes

--- src/main/java/com/example/demowithtests/domain/Employee.java
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
