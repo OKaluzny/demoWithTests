80f7fafdddbe07f04e17907e5787f8c438e3a968
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-20 00:21:16 +0300

    add new tests

--- src/test/java/com/example/demowithtests/ServiceTests.java
+++ src/test/java/com/example/demowithtests/ServiceTests.java
@@ -14,9 +14,12 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -63,9 +66,72 @@ public class ServiceTests {
 
     @Test(expected = EntityNotFoundException.class)
     public void should_throw_exception_when_employee_doesnt_exist() {
-        Employee testEmployee = Employee.builder().id(89).name("Mark").build();
+        Employee testEmployee = Employee.builder().id(89).name("Max").build();
 
         given(repository.findById(anyInt())).willReturn(Optional.empty());
         service.getById(testEmployee.getId());
     }
+
+    @Test
+    public void whenGivenName_shouldTestEmployeeList(){
+        Employee testEmployee = Employee.builder().name("Max").build();
+        List<Employee> testEmployeeList = new ArrayList<>();
+        testEmployeeList.add(testEmployee);
+
+        when(sqlRepository.findUserByName(testEmployee.getName())).thenReturn(testEmployeeList);
+        List<Employee> expectedTestEmployeeList = service.findUserByName(testEmployee.getName());
+
+        assertThat(expectedTestEmployeeList.get(0).getName()).isEqualTo("Max");
+    }
+
+    @Test
+    public void whenGivenEmail_shouldTestEmployeeList(){
+        Employee testEmployee = Employee.builder().email("max@gmail.com").build();
+        List<Employee> testEmployeeList = new ArrayList<>();
+        testEmployeeList.add(testEmployee);
+
+        when(sqlRepository.findEmployeeByEmail(testEmployee.getEmail())).thenReturn(testEmployeeList);
+        List<Employee> expectedTestEmployeeList = service.findEmployeeByEmail(testEmployee.getEmail());
+
+        assertThat(expectedTestEmployeeList.get(0).getEmail()).isEqualTo("max@gmail.com");
+    }
+
+    @Test
+    public void whenGivenIsDeletedField_shouldTestEmployeeList(){
+        Employee testEmployee = Employee.builder().isDeleted(true).build();
+        List<Employee> testEmployeeList = new ArrayList<>();
+        testEmployeeList.add(testEmployee);
+
+        when(sqlRepository.findAllByIsDeleted(testEmployee.getIsDeleted())).thenReturn(testEmployeeList);
+        List<Employee> expectedTestEmployeeList = service.findAllByIsDeleted(testEmployee.getIsDeleted());
+
+        assertThat(expectedTestEmployeeList.get(0).getIsDeleted()).isEqualTo(true);
+    }
+
+    @Test
+    public void whenGivenIsAdultField_shouldTestEmployeeList(){
+        Employee testEmployee = Employee.builder().isAdult(true).build();
+        List<Employee> testEmployeeList = new ArrayList<>();
+        testEmployeeList.add(testEmployee);
+
+        when(jpqlRepository.findAdultUser(testEmployee.getIsAdult())).thenReturn(testEmployeeList);
+        List<Employee> expectedTestEmployeeList = service.findAdultUser(testEmployee.getIsAdult());
+
+        assertThat(expectedTestEmployeeList.get(0).getIsAdult()).isEqualTo(true);
+    }
+
+    @Test
+    public void whenGivenCountry_shouldTestEmployeeList(){
+        Employee testEmployee = Employee.builder().country("Ukraine").build();
+        List<Employee> testEmployeeList = new ArrayList<>();
+        testEmployeeList.add(testEmployee);
+
+        when(jpqlRepository.findEmployeeByCountry(testEmployee.getCountry())).thenReturn(testEmployeeList);
+        List<Employee> expectedTestEmployeeList = service.findEmployeeByCountry(testEmployee.getCountry());
+
+        assertThat(expectedTestEmployeeList.get(0).getCountry()).isEqualTo("Ukraine");
+    }
+
+
+
 }
