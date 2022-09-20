d1b7b5e7536e5d9581263081ce106f5e2fef5155
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-20 14:37:53 +0300

    add to folder

--- src/main/java/com/example/demowithtests/util/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/example/demowithtests/util/exeption/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.demowithtests.util;
+package com.example.demowithtests.util.exeption;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/example/demowithtests/util/exeption/ResourceNotFoundException.java
+++ src/main/java/com/example/demowithtests/util/exeption/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.demowithtests.util;
+package com.example.demowithtests.util.exeption;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
