633411364f8a54e82a750ef6c1f31446b4f05c10
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-22 19:22:46 +0300

    add Dto

---
+++ src/main/java/com/example/demowithtests/dto/updateDto/EmployeeUpdateCountryDto.java
@@ -0,0 +1,26 @@
+package com.example.demowithtests.dto.updateDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class EmployeeUpdateCountryDto {
+    @NotNull(message = "Name may not be null")
+    @Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
+    @Schema(description = "Name of an employee.", example = "Billy", required = true)
+    public String name;
+
+    @Schema(description = "Name of the country.", example = "England", required = true)
+    public String country;
+
+    @Email
+    @NotNull
+    @Schema(description = "Email address of an employee.", example = "billys@mail.com", required = true)
+    public String email;
+
+    @NotNull(message = "Age may not be null")
+    @Schema(description = "Age of an employee.", example = "28", required = true)
+    public Integer age;
+}
