49e12869fd1a80786ff3b4960d2582132afbec60
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-09-25 00:16:27 +0300

    refactoring

--- src/main/java/com/example/demowithtests/web/interfaces/patch/PatchUpdateNameRequest.java
+++ src/main/java/com/example/demowithtests/web/interfaces/patch/PatchUpdateNameRequest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface PatchUpdateNameRequest {
-    @Operation(summary = "This is endpoint allows to update value of field isDeleted.", description = "Create request to update value of field isDeleted.", tags = {"Employee"})
+    @Operation(summary = "This is endpoint allows to update name by id.", description = "Create request to update to update name by id.", tags = {"Employee"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "OK. pam pam param."),
             @ApiResponse(responseCode = "400", description = "Invalid input"),
