bce6a708d0b4a2eafa5a85cc21d257bf3219c53e
Artem Kovalov <artemkov1990@gmail.com>
2023-06-09 12:16:53 +0300

    Added EmployeeNotFoundException and handler

---
+++ src/main/java/com/example/demowithtests/util/exception/ServiceExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.demowithtests.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import static com.example.demowithtests.util.exception.ErrorDetails.getResponseEntity;
+
+@ControllerAdvice
+public class ServiceExceptionHandler {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    protected ResponseEntity<ErrorDetails> handleDeleteException(WebRequest request) {
+        return getResponseEntity("This user was deleted", request, HttpStatus.NOT_FOUND);
+    }
+
+}
