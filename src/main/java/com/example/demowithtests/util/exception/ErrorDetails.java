019a3d1d6808516395ff49d77e6292595cb118b6
Artem Kovalov <artemkov1990@gmail.com>
2023-06-09 12:00:26 +0300

    Added Lombok annotations and getResponseEntity() static method

--- src/main/java/com/example/demowithtests/util/exception/ErrorDetails.java
+++ src/main/java/com/example/demowithtests/util/exception/ErrorDetails.java
@@ -1,28 +1,23 @@
 package com.example.demowithtests.util.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
 import java.util.Date;
 
+@Getter
+@RequiredArgsConstructor
 public class ErrorDetails {
-    private Date timestamp;
-    private String message;
-    private String details;
 
-    public ErrorDetails(Date timestamp, String message, String details) {
-        super();
-        this.timestamp = timestamp;
-        this.message = message;
-        this.details = details;
-    }
+    private final Date timestamp;
+    private final String message;
+    private final String details;
 
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getDetails() {
-        return details;
+    public static ResponseEntity<ErrorDetails> getResponseEntity(String message, WebRequest request, HttpStatus status) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), message, request.getDescription(true));
+        return new ResponseEntity<>(errorDetails, status);
     }
 }
