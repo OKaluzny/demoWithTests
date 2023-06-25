2d28464b3b15d0a2224ff2eb83a16da1696e25f0
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-06-25 20:33:47 +0300

    Changed logic of isValid()

--- src/main/java/com/example/demowithtests/util/annotations/dto/BlockedEmailDomainsValidator.java
+++ src/main/java/com/example/demowithtests/util/annotations/dto/BlockedEmailDomainsValidator.java
@@ -1,10 +1,7 @@
 package com.example.demowithtests.util.annotations.dto;
 
-import lombok.NonNull;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraints.NotNull;
 import java.util.Arrays;
 
 public class BlockedEmailDomainsValidator implements ConstraintValidator<BlockedEmailDomains, String> {
@@ -22,30 +19,11 @@ public class BlockedEmailDomainsValidator implements ConstraintValidator<Blocked
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         if (email != null) {
-            String emailDomain = email.substring(email.lastIndexOf('@') +1);
+            String emailDomain = email.substring(email.lastIndexOf('@') + 1);
             return Arrays.stream(endings).noneMatch(emailDomain::endsWith) &&
                     Arrays.stream(domains).noneMatch(emailDomain::contains);
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        String[] emails = {null, "yandru@yandex.ru", "yandua@yandex.ua", "mailru@mail.ru", "ukr@ukr.com", "vkua@vk.com", "ukr@ukr.рф"};
-        String[] domains = {"yandex", "vk"};
-        String[] endings = {".com1", ".ru", ".su", ".ру", ".рф"};
-        for (String email : emails) {
-            String emailDomain = null;
-            boolean b = false;
-            if (email != null) {
-                emailDomain = email.substring(email.lastIndexOf('@') +1);
-                b = Arrays.stream(endings).noneMatch(emailDomain::endsWith) &&
-                        Arrays.stream(domains).noneMatch(emailDomain::contains);
-            }
-            System.out.println("Email: " + email + " | " + "Domain: " + emailDomain + " | " + "Boolean 1: " + b);// + " | " + "Boolean 2: " + b1);
-//            boolean b1 = Arrays.stream(endings).noneMatch(emailDomain::endsWith);
-        }
-
-
-
-    }
 }
--- src/main/java/com/example/demowithtests/util/annotations/dto/CountryRightFormedValidator.java
+++ src/main/java/com/example/demowithtests/util/annotations/dto/CountryRightFormedValidator.java
@@ -7,8 +7,6 @@ public class CountryRightFormedValidator implements ConstraintValidator<CountryR
 
     @Override
     public boolean isValid(String country, ConstraintValidatorContext constraintValidatorContext) {
-        if (country == null)
-            return true;
-        return country.length() == 2 && country.equals(country.toUpperCase());
+        return country != null && country.length() == 2 && country.equals(country.toUpperCase());
     }
 }
