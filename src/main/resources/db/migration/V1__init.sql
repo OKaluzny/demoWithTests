7b35362d368348ef8a05d67f05332d0df25b8a8a
OKaluzny <kaluzny.oleg@gmail.com>
2023-07-12 17:18:32 +0300

    some changes into scripts

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,21 +1,24 @@
-create table public.users (
-                              id integer primary key not null default nextval('users_id_seq'::regclass),
-                              name character varying,
-                              email character varying,
-                              country character varying,
-                              is_deleted boolean
+create table if not exists public.users
+(
+    id         serial
+        primary key,
+    name       varchar(255),
+    email      varchar(255),
+    country    varchar(255),
+    is_deleted boolean default false
 );
-create unique index users_id_uindex on users using btree (id);
 
-create table public.addresses (
-                                  id bigint primary key not null default nextval('addresses_id_seq'::regclass),
-                                  address_has_active boolean,
-                                  city character varying(255),
-                                  country character varying(255),
-                                  street character varying(255),
-                                  employee_id integer,
-                                  foreign key (employee_id) references public.users (id)
-                                      match simple on update no action on delete no action
+create table if not exists public.addresses
+(
+    id                 serial
+        primary key,
+    address_has_active boolean default false,
+    city               varchar(255),
+    country            varchar(255),
+    street             varchar(255),
+    employee_id        integer
+        constraint fk_address_employee references public.users
+
 );
 
 
